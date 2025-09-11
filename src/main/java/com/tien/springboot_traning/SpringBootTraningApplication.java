1663d585b8558b42a2e642001b3cd0383ebf0dd5
nqtienvn <quangtiencoder1876@gmail.com>
2025-09-11 22:00:32 +0700

    first commit

---
+++ src/main/java/com/tien/springboot_traning/SpringBootTraningApplication.java
@@ -0,0 +1,13 @@
+package com.tien.springboot_traning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTraningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTraningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tien/springboot_traning/SpringBootTraningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tien.springboot_traning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTraningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
